9541942d26f61464ba141f70495cdf3d1efcc693
RikuMesiki <147460971+RikuMesiki@users.noreply.github.com>
2024-09-03 19:40:14 +0800

    Update cache.c

--- cache.c
+++ cache.c
@@ -1,1 +1,36 @@
+#include <string.h>
+#include "cache.h"
 
+static Entry cache[MAX_ENTRIES];
+static int count = 0;
+
+void init() {
+    count = 0;
+    for (int i = 0; i < MAX_ENTRIES; i++) {
+        cache[i].size = 0;
+        cache[i].url[0] = '\0';
+    }
+}
+
+void save(const char *url, const char *data, int size) {
+    if (size > MAX_OBJECT_SIZE || size + count > MAX_CACHE_SIZE) {
+        return;
+    }
+
+    if (count < MAX_ENTRIES) {
+        strcpy(cache[count].url, url);
+        memcpy(cache[count].data, data, size);
+        cache[count].size = size;
+        count++;
+    }
+}
+
+int load(const char *url, char *data) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(cache[i].url, url) == 0) {
+            memcpy(data, cache[i].data, cache[i].size);
+            return cache[i].size;
+        }
+    }
+    return 0;
+}
